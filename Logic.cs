9b78d62b3d8fbdc13926ad0c201bf0f1efd296d8
Jeff <jeckman03@gmail.com>
2021-09-21 22:55:47 +0000

    Initial commit

---
+++ Logic.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public static class Logic
+{
+	// Add the weight for a set
+
+	// Adjust the reps in a set
+
+	// Switch a lift out for another
+
+	// Complete a workout
+	
+}
